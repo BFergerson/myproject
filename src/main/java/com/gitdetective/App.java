b0d8a9bbddef080d46e6b13eb8f4cc49bbd38851
Brandon Fergerson <fergerson92@gmail.com>
2018-04-20 21:20:34 -0400

    Update App.java

--- src/main/java/com/gitdetective/App.java
+++ src/main/java/com/gitdetective/App.java
@@ -8,5 +8,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    public static void internalCall() {
+        MyClass yay = new MyClass();
+        yay.myMethod();
+    }
 
 }
