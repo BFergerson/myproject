db6e2fe9c5de173dc34187b0ee828a8c0a5ee312
Brandon Fergerson <fergerson92@gmail.com>
2018-06-30 10:13:09 -0400

    some changes

--- src/main/java/com/gitdetective/App.java
+++ src/main/java/com/gitdetective/App.java
@@ -8,36 +8,46 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hi2");
+        System.out.println(doThing(2));
     }
     
     public static void internalCall1() {
         System.out.println("hi2");
         MyClass yay = new MyClass();
         yay.myMethod();
+        System.out.println(doThing(2));
     }
     
     public static void internalCall2() {
         System.out.println("hi2");
         MyClass yay = new MyClass();
         yay.myMethod2();
+        System.out.println(doThing(2));
     }
     
     public static void internalCall3() {
         System.out.println("hi2");
         MyClass yay = new MyClass();
         yay.myMethod2();
+        System.out.println(doThing(2));
     }
     
     public static void internalCall4() {
         System.out.println("hi2");
         MyClass yay = new MyClass();
         yay.myMethod2();
+        System.out.println(doThing(2));
     }
     
     public static void internalCall5() {
         System.out.println("hi2");
         MyClass yay = new MyClass();
         yay.myMethod2();
+        System.out.println(doThing(2));
+    }
+
+    private static int doThing(int x) {
+        return x * x;
     }
 
 }
