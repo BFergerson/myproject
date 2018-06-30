8e5c867e951145d85306af9362efb6de67079fa4
Brandon Fergerson <fergerson92@gmail.com>
2018-06-30 10:04:18 -0400

    some changes

--- src/main/java/com/gitdetective/App.java
+++ src/main/java/com/gitdetective/App.java
@@ -7,29 +7,35 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("hi");
     }
     
     public static void internalCall1() {
+        System.out.println("hi");
         MyClass yay = new MyClass();
         yay.myMethod();
     }
     
     public static void internalCall2() {
+        System.out.println("hi");
         MyClass yay = new MyClass();
         yay.myMethod2();
     }
     
     public static void internalCall3() {
+        System.out.println("hi");
         MyClass yay = new MyClass();
         yay.myMethod2();
     }
     
     public static void internalCall4() {
+        System.out.println("hi");
         MyClass yay = new MyClass();
         yay.myMethod2();
     }
     
     public static void internalCall5() {
+        System.out.println("hi");
         MyClass yay = new MyClass();
         yay.myMethod2();
     }
